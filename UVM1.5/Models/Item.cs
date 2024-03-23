0fc4bfe373b4d2932eabac75e30356ff136c7240
Teggenberg <tdeggenberger@student.fullsail.edu>
2024-03-23 13:33:27 -0500

    serial number parsing function

---
+++ UVM1.5/Models/Item.cs
@@ -0,0 +1,17 @@
+﻿namespace UVM1._5.Models
+{
+    public class Item
+    {
+        public int? Id { get; set; }
+        public int? Location { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string? Serial { get; set; }
+        public int? Condition { get; set; }
+        public float?  Retail {  get; set; }
+        public float? Cost { get; set; }
+        public string? Description { get; set; }
+        public List<string>? Details { get; set; }
+
+    }
+}
